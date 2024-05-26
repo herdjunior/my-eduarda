9a1cb2b9901de90d54dc7a2416967ca788547256
herdjunior <herdesonaraujo.junior@gmail.com>
2024-05-26 14:16:19 -0400

    teste

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold margin my-2">Contrato de relacionamento N 01</h1>
+        <h3 className="mb-2">Dever das partes:</h3>
+        <p>Herdeson de Araujo Queiroz Junior</p>
+        <p>1. Prometo de contar tudo, mesmo que doa, a fim de manter a verdade, seja boa ou seja ruim.</p>
+        <p>2. Te amar pra sempre porque você me ajuda muito e porque acredito em nós.</p>
+        <p className="mb-2">3. Sempre te ouvir e te perdoar.</p>
+        <p>Maria Eduarda Miranda Reis</p>
+        <p>1. Prometo de contar tudo, mesmo que doa, a fim de manter a verdade, seja boa ou seja ruim.</p>
+        <p>2. Te amar pra sempre porque você me ajuda muito e porque acredito em nós.</p>
+        <p>3. Sempre te ouvir e te perdoar.</p>
+        <a className="mt-4 border-solid border-lime-500 border-2 rounded-sm bg-lime-500 font-bold" href="https://wa.me/5592984831099">Confirmar assinatura de contrato</a>
+      </div>
+    </div>
+  );
+}
